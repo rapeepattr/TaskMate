774ce97cd6b0745e0bffb5ef5e78a2264dece490
rapeepattr <rapeepat.ptpk@gmail.com>
2024-01-20 21:00:11 +0700

    Remove useDebugValue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useDebugValue, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
